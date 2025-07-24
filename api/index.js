83dff6dcc70c3a7b59ebe97af03fe4956b04875e
AlimOmar <alimjoo00@163.com>
2025-07-24 23:52:38 +0800

    first commit

---
+++ api/index.js
@@ -0,0 +1,6 @@
+
+import { translate } from '@vitalets/google-translate-api';
+
+const { text } = await translate('Привет, мир! Как дела?', { to: 'en' });
+
+console.log(text) // => 'Hello World! How are you?'
